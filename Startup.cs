7fec2dbf41d1abd71ab189d8651b1c436bae2584
Laura Tinney <lauratinn3y@hotmail.co.uk>
2021-07-05 16:56:06 +0100

    Update url for azure

--- Startup.cs
+++ Startup.cs
@@ -33,8 +33,8 @@ namespace BapBlazor
             
             services.AddScoped<HttpClient>(s =>
             {
-                // return new HttpClient { BaseAddress = new Uri(@"https://bapapisqlite.azurewebsites.net/") };
-                return new HttpClient { BaseAddress = new Uri(@"http://localhost:5002/") };
+                return new HttpClient { BaseAddress = new Uri(@"https://team2-api.azurewebsites.net") };
+                // return new HttpClient { BaseAddress = new Uri(@"http://localhost:5002/") };
             });
         }
 
